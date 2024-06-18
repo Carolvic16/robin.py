3b3ff4895f3cb7b80a1340dd56642959c71f306b
Victória Carolina Ferreira Da Silva <122389604+Carolvic16@users.noreply.github.com>
2024-06-18 20:42:08 -0300

    não sei se isso ta certo mas fé

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import threading
 import time 
 from threading import Event, Thread 
 
-print()
+
+## essa função limita a parcela de tempo, então se o processo entar  no tempo, sera menor valor 
+def tempo_quantum (quantum, valor_min = 1, valor_max= 5 ):
+  if (quantum < valor_min ):
+    return valor_min
+  elif (quantum > valor_min): 
+    return valor_max 
+  else: 
+    return quantum
---
+++ processos.py
@@ -0,0 +1,21 @@
+def criar_array(tamanho):
+    array = []
+    for i in range(tamanho):
+        valor = float(input(f"Entre com o valor da tarefa {i+1}: "))
+        array.append(valor)
+    return array
+
+# Definir o tamanho do array
+tarefas = int(input("Entre com a quantidade de processos: "))
+
+# Criar o array com os valores do usuário
+meu_array = criar_array(tarefas)
+
+# Adicionar o último número ao array e estender a lista com esse número
+if meu_array:
+    ultimo_valor = meu_array[-1]
+    meu_array.append(ultimo_valor)  # Adiciona o último número
+    meu_array.extend([ultimo_valor])  # Estende a lista com o último número
+
+# Exibir o array
+print("Os valores do array são:", meu_array)
